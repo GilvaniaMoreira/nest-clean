6e2eb1aba5cb52f9cd8280e635e913916abca1d2
gilvania moreira <moreiragil84@gmail.com>
2023-12-08 18:10:25 -0300

    feat: hash password

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -6,6 +6,7 @@ import {
     Post,
   } from '@nestjs/common'
   import { PrismaService } from 'src/prisma/prisma.server'
+  import { hash } from 'bcryptjs'
   
   @Controller('/accounts')
   export class CreateAccountController {
@@ -28,11 +29,13 @@ import {
         )
       }
   
+      const hashedPassword = await hash(password, 8)
+
       await this.prisma.user.create({
         data: {
           name,
           email,
-          password,
+          password: hashedPassword,
         },
       })
     }
